f080193643f2e374dec964361c3a77d9c11020ee
Samylaneres <samyaneres13@gmail.com>
2024-09-11 14:47:50 -0300

    positivo e negativo

---
+++ atividade13.py
@@ -0,0 +1,11 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+numero = float(input("Digite um numero : "))
+
+if numero > 0:
+    print("Positivo")
+elif numero < 0:
+    print("Negativo")
+else:
+    print("Zero")
+
+    
